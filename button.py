3d97d00c96e48cf6c88868842579e526d7f5e75d
Christopher Contreras <112120990+upnextcontreras@users.noreply.github.com>
2024-10-11 14:39:51 -0700

    Update button.py

--- button.py
+++ button.py
@@ -1,18 +1,23 @@
 import pygame.font
 
 class Button:
-    def __init__(self, ai_game, msg):
+    def __init__(self, ai_game, msg, y_offset=0):
+        """Initialize button attributes."""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
+        # Button size and properties
         self.width, self.height = 200, 50
         self.button_color = (0, 135, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
+        # Create button rect and set position
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
+        self.rect.centery += y_offset  # Apply the y_offset to position the button
 
+        # Prepare the button's message
         self._prep_msg(msg)
 
     def reset_message(self, msg="Play"):
@@ -20,11 +25,13 @@ class Button:
         self._prep_msg(msg)
 
     def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
         self.msg_image = self.font.render(msg, True, self.text_color,
-                self.button_color)
+                                          self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
+        """Draw button and then draw message."""
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
