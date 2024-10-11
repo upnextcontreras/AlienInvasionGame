53727a4214202314bc64371fbb8e34f03f6f2ca0
Christopher Contreras <112120990+upnextcontreras@users.noreply.github.com>
2024-10-11 16:34:55 -0700

    Update alien.py

--- alien.py
+++ alien.py
@@ -11,20 +11,17 @@ class Alien(Sprite):
     alien_images1 = [pg.image.load(f"images/alien1{n}.png") for n in range(2)]
     alien_images2 = [pg.image.load(f"images/alien2{n}.png") for n in range(2)]
     alien_images = [alien_images0, alien_images1, alien_images2]
-    # alien_explosion_images = []  # fill in explosion images here
 
     def __init__(self, ai_game, v): 
         super().__init__()
         self.ai_game = ai_game
         self.screen = ai_game.screen
-        self.v = v
+        self.v = v  # Base velocity of the alien
 
         type = randint(0, 2)
         self.timer = Timer(images=Alien.alien_images[type], delta=(type+1)*600, start_index=type % 2)
-        # self.explosion_timer = Timer(images=Alien.alien.explosion_images, start_index=Alien.n % 2,
-        #                              loop_continuously=False)
+
         self.image = self.timer.current_image()
-        print(self.image)
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
@@ -40,12 +37,18 @@ class Alien(Sprite):
         sr = self.screen.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
-        r = self.rect 
         return self.x + self.rect.width >= sr.right or self.x <= 0
 
     def update(self):
-        self.x += self.v.x
-        self.y += self.v.y
+        """Update the alien's position and adjust its speed dynamically."""
+        remaining_aliens = len(self.ai_game.fleet.aliens)  # Get the number of remaining aliens
+
+        # Increase velocity as the number of remaining aliens decreases
+        speed_multiplier = max(1, 5 - remaining_aliens // 10)  # Example logic: speed up as the number of aliens drops
+
+        self.x += self.v.x * speed_multiplier
+        self.y += self.v.y * speed_multiplier
+
         self.image = self.timer.current_image()
         self.draw()
 
