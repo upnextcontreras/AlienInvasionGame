ff46b7259339d04c6b5b0d8cc9dc9e6bf0d8c520
Christopher Contreras <112120990+upnextcontreras@users.noreply.github.com>
2024-10-11 18:39:44 -0700

    Update sound.py

--- sound.py
+++ sound.py
@@ -6,21 +6,30 @@ class Sound:
     def __init__(self): 
         self.pickup = pg.mixer.Sound('sounds/pickup.wav')
         self.gameover = pg.mixer.Sound('sounds/gameover.wav')
-        pg.mixer.music.load('sounds/FE.mp3')
-        pg.mixer.music.set_volume(0.2)
-                                             
+
+        # Load different versions of the background music at different speeds
+        self.normal_music = 'sounds/FE.mp3'
+        self.fast_music = 'sounds/FE1.mp3'
+        self.faster_music = 'sounds/FE2.mp3'
+
+        self.current_music = self.normal_music  # Start with the normal music
+        self.music_playing = False
+
     def play_background(self): 
-        pg.mixer.music.play(-1, 0.0)
+        pg.mixer.music.load(self.current_music)
+        pg.mixer.music.set_volume(0.2)
+        pg.mixer.music.play(-1, 0.0)  # Loop indefinitely
         self.music_playing = True
         
     def play_pickup(self): 
-        if self.music_playing: self.pickup.play()
+        if self.music_playing: 
+            self.pickup.play()
         
     def play_gameover(self):
         if self.music_playing: 
             self.stop_background()
             self.gameover.play()
-            time.sleep(3.0)       # sleep until game over sound has finished
+            time.sleep(3.0)  # Sleep until the game over sound has finished
         
     def toggle_background(self):
         if self.music_playing: 
@@ -32,6 +41,27 @@ class Sound:
     def stop_background(self): 
         pg.mixer.music.stop()
         self.music_playing = False 
+    
+    def adjust_music_speed(self, remaining_aliens, total_aliens):
+        """Adjust background music based on the number of remaining aliens."""
+        percentage = remaining_aliens / total_aliens
+        
+        if percentage > 0.6:
+            # More than 60% of aliens remain, play normal music
+            if self.current_music != self.normal_music:
+                self.current_music = self.normal_music
+                self.play_background()  # Restart music with the correct version
+        elif 0.3 < percentage <= 0.6:
+            # Between 30% and 60% aliens remain, play fast music
+            if self.current_music != self.fast_music:
+                self.current_music = self.fast_music
+                self.play_background()  # Restart music with the correct version
+        else:
+            # Less than 30% aliens remain, play faster music
+            if self.current_music != self.faster_music:
+                self.current_music = self.faster_music
+                self.play_background()  # Restart music with the correct version
+
     
         
     
